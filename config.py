270bbb746bc5c2b05893a7349783213834c1091c
pm4617 <mahajanparesh@gmail.com>
2021-10-28 17:03:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_KEY = 'youralpacaapikey'
 API_SECRET = 'youralpacaapisecret'
 
-WEBHOOK_PASSPHRASE = 'yourpassphrase'
+WEBHOOK_PASSPHRASE = 'password'
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
